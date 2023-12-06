5a730cf66ccc46b038dcae0e491b80984caf5fd1
Nur Muhammad Herlim <99372078+aamu14@users.noreply.github.com>
2023-12-07 04:04:59 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import subprocess
 import pandas as pd
+import matplotlib.pyplot as plt
+from PIL import Image
 
 st.header('🎈 R x Python Streamlit App')
 
@@ -38,6 +40,31 @@ st.markdown("""
            Dari tabel di atas dapat dilihat bahwa terdapat satu kecamatan yang luasnya sangat kecil, yaitu 2,03 km^2, sedangkan daerah yang paling luas memiliki ukuran sebesar 24,24 km^2.  Selanjutnya, kepadatan penduduk yang paling kecil bernilai 3336 orang/km^2 dan nilai paling besar yaitu 22018 orang/km^2.  Selain itu, terdapat indikasi pencilan (outlier) pada beberapa variabel yang dapat dilihat dari jauhnya rentang nilai minimum ke nilai maksimum dari masing-masing variabel.  Pencilan ini dapat mempengaruhi keakuratan analisis. Tetapi, tidak dilakukan penghapusan data karena semua kecamatan harus dipertimbangkan.
            """)
 
+
+# Assuming your data is in a CSV file named 'data.csv'
+data = pd.read_csv("datatest.csv")
+
+# Sorting the DataFrame by 'Tingkat TK' column in descending order
+sorted_data = data.sort_values(by='Tingkat TK/RA', ascending=False)
+
+# Creating a histogram using Matplotlib
+plt.figure(figsize=(10, 6))  # Adjust the figure size as needed
+bars = plt.bar(sorted_data['Kecamatan'], sorted_data['Tingkat TK/RA'])
+plt.xlabel('Kecamatan')
+plt.ylabel('Tingkat TK')
+plt.title('Histogram of Tingkat TK by Kecamatan')
+plt.xticks(rotation=90)  # Rotate x-axis labels for better readability if needed
+
+# Adding values on top of the bars
+for bar in bars:
+    yval = bar.get_height()
+    plt.text(bar.get_x() + bar.get_width() / 2, yval, round(yval, 2), va='bottom', ha='center')
+
+plt.tight_layout()
+# Saving the histogram as an image
+plt.savefig('graph_1.png')  # Save the image as 'graph_1.png'
+# Displaying the histogram
+plt.show()
 
 
 st.subheader('1. Printing text in R')
