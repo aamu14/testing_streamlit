a02bd10b829de926b06a6373dbf297a58fa0e992
Nur Muhammad Herlim <99372078+aamu14@users.noreply.github.com>
2023-12-07 02:11:00 +0700

    Create bagian 1 kmeans.R

---
+++ bagian 1 kmeans.R
@@ -0,0 +1,14 @@
+library(readxl)
+library(cluster)
+library(factoextra)
+library(fpc)
+library(clusterSim)
+
+# Load the openxlsx package
+library(openxlsx)
+# Read the data file
+df <- read_excel("datatest.xlsx")
+
+# Create a summary of the data
+summary_data <- summary(df)
+summary_data
