73e6acbe81789f29b877288e66cdb0315360a800
Nur Muhammad Herlim <99372078+aamu14@users.noreply.github.com>
2023-12-07 04:38:55 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import subprocess
 import pandas as pd
 import matplotlib.pyplot as plt
-
+from PIL import Image
 st.header('🎈 R x Python Streamlit App')
 
 st.sidebar.markdown('''
