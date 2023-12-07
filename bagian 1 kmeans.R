e853da5c90597cf84027400f0048e53cd0cbf8c8
Nur Muhammad Herlim <99372078+aamu14@users.noreply.github.com>
2023-12-07 14:18:01 +0700

    Update bagian 1 kmeans.R

--- bagian 1 kmeans.R
+++ bagian 1 kmeans.R
@@ -21,8 +21,6 @@ clusterCut_3<-cutree(hc.c,3)
 clusterCut_4<-cutree(hc.c,4)
 clusterCut_5<-cutree(hc.c,5)
 
-library(cluster)
-
 sk2 <-silhouette(clusterCut_2,distance)
 sk3 <-silhouette(clusterCut_3,distance)
 sk4 <-silhouette(clusterCut_4,distance)
@@ -45,3 +43,33 @@ png("sk5_plot.png")
 plot(sk5)
 dev.off()
 
+#kmeans
+kc2<- kmeans(z,2)
+kc3<- kmeans(z,3)
+kc4<- kmeans(z,4)
+kc5<- kmeans(z,5)
+clusterCut_2<-kc2
+clusterCut_3<-kc3
+clusterCut_4<-kc4
+clusterCut_5<-kc5
+
+sk6 <-silhouette(clusterCut_2,distance)
+sk7 <-silhouette(clusterCut_3,distance)
+sk8 <-silhouette(clusterCut_4,distance)
+sk9 <-silhouette(clusterCut_5,distance)
+
+png("sk6_plot.png")
+plot(sk6)
+dev.off()
+
+png("sk7_plot.png")
+plot(sk7)
+dev.off()
+
+png("sk8_plot.png")
+plot(sk8)
+dev.off()
+
+png("sk9_plot.png")
+plot(sk9)
+dev.off()
