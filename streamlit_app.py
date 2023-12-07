aa5c503c6a4290bd9eb6eed9028f14480d45ddbf
Nur Muhammad Herlim <99372078+aamu14@users.noreply.github.com>
2023-12-07 13:30:50 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -260,6 +260,5 @@ distance
     st.code(code1, language='R')
 process1 = subprocess.Popen(["Rscript", "bagian 1 kmeans.R"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
 result1 = process1.communicate()
-images = ['sk2_plot.png','sk3_plot.png', 'sk4_plot.png', 'sk5_plot.png']
-image = Image.open(images)
+image = Image.open(['sk2_plot.png','sk3_plot.png', 'sk4_plot.png', 'sk5_plot.png'])
 st.image(image)
