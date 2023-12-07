e524b91c6968b06fbb48f0e10967e572f5cbb996
Nur Muhammad Herlim <99372078+aamu14@users.noreply.github.com>
2023-12-07 12:41:50 +0700

    Update bagian 1 kmeans.R

--- bagian 1 kmeans.R
+++ bagian 1 kmeans.R
@@ -12,3 +12,20 @@ df <- df[,-1]
 z <- scale(df)
 # Calculate Euclidean distance
 distance <- dist(z)
+#set the cluster we needed. in this case, i use cluster between 2 to 5.
+kc2<- kmeans(z,2)
+kc3<- kmeans(z,3)
+kc4<- kmeans(z,4)
+kc5<- kmeans(z,5)
+
+clusterCut_2<-kc2
+clusterCut_3<-kc3
+clusterCut_4<-kc4
+clusterCut_5<-kc5
+
+#use dunn index
+library(clValid)
+dunn(distance, clusterCut_2$cluster)
+dunn(distance, clusterCut_3$cluster)
+dunn(distance, clusterCut_4$cluster)
+dunn(distance, clusterCut_5$cluster)
