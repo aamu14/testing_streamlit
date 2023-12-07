0cd967c48b739c37d8708f2f217dc5661b47cd2a
Nur Muhammad Herlim <99372078+aamu14@users.noreply.github.com>
2023-12-07 13:29:27 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -257,7 +257,9 @@ z <- scale(Data_Per_Kecamatan_Bandar_Lampung[,-1])
 distance <- dist(z)
 distance
     '''
-    st.code(code1, language='r')
+    st.code(code1, language='R')
 process1 = subprocess.Popen(["Rscript", "bagian 1 kmeans.R"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
 result1 = process1.communicate()
-st.dataframe(result1)
+images = ['sk2_plot.png','sk3_plot.png', 'sk4_plot.png', 'sk5_plot.png']
+image = Image.open(images)
+st.image(image)
