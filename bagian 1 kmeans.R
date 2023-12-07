9560ea2bc07a6384458f095c1de30d208a5aa3f5
Nur Muhammad Herlim <99372078+aamu14@users.noreply.github.com>
2023-12-07 14:27:24 +0700

    Update bagian 1 kmeans.R

--- bagian 1 kmeans.R
+++ bagian 1 kmeans.R
@@ -26,20 +26,6 @@ sk3 <-silhouette(clusterCut_3,distance)
 sk4 <-silhouette(clusterCut_4,distance)
 sk5 <-silhouette(clusterCut_5,distance)
 
-#kmeans
-kc2<- kmeans(z,2)
-kc3<- kmeans(z,3)
-kc4<- kmeans(z,4)
-kc5<- kmeans(z,5)
-clusterCut_6<-kc2
-clusterCut_7<-kc3
-clusterCut_8<-kc4
-clusterCut_9<-kc5
-
-sk6 <-silhouette(clusterCut_6,distance)
-sk7 <-silhouette(clusterCut_7,distance)
-sk8 <-silhouette(clusterCut_8,distance)
-sk9 <-silhouette(clusterCut_9,distance)
 
 # Open PNG devices to save the plots
 png("sk2_plot.png")
@@ -56,20 +42,4 @@ dev.off()
 
 png("sk5_plot.png")
 plot(sk5)
-dev.off()
-
-png("sk6_plot.png")
-plot(sk6)
-dev.off()
-
-png("sk7_plot.png")
-plot(sk7)
-dev.off()
-
-png("sk8_plot.png")
-plot(sk8)
-dev.off()
-
-png("sk9_plot.png")
-plot(sk9)
 dev.off()
