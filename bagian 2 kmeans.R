45d500defaba44ef5cf62e2909cc801de2ddd835
Nur Muhammad Herlim <99372078+aamu14@users.noreply.github.com>
2023-12-07 14:26:02 +0700

    Create bagian 2 kmeans.R

---
+++ bagian 2 kmeans.R
@@ -0,0 +1,42 @@
+library(readxl)
+library(cluster)
+library(factoextra)
+library(fpc)
+library(knitr)
+
+# Get content into a data frame
+df <- read.csv("datatest.csv",
+                header = TRUE, sep = ",")
+df <- df[,-1]
+# Scale the data
+z <- scale(df)
+#kmeans
+kc2<- kmeans(z,2)
+kc3<- kmeans(z,3)
+kc4<- kmeans(z,4)
+kc5<- kmeans(z,5)
+clusterCut_6<-kc2
+clusterCut_7<-kc3
+clusterCut_8<-kc4
+clusterCut_9<-kc5
+
+sk6 <-silhouette(clusterCut_6,distance)
+sk7 <-silhouette(clusterCut_7,distance)
+sk8 <-silhouette(clusterCut_8,distance)
+sk9 <-silhouette(clusterCut_9,distance)
+
+png("sk6_plot.png")
+plot(sk6)
+dev.off()
+
+png("sk7_plot.png")
+plot(sk7)
+dev.off()
+
+png("sk8_plot.png")
+plot(sk8)
+dev.off()
+
+png("sk9_plot.png")
+plot(sk9)
+dev.off()
