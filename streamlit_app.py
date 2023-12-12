f27155607335aeec36aecaab1f5e5d0d7345677b
Nur Muhammad Herlim <99372078+aamu14@users.noreply.github.com>
2023-12-12 21:19:28 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -70,7 +70,7 @@ summary_stats = data_summary.describe().loc[['min', '25%', '50%', '75%', 'mean',
 summary_stats = summary_stats.rename(index={'min': 'Min', 'mean': 'Mean', 'max': 'Max', '25%': 'Q1', '50%': 'Median', '75%': 'Q3'})
 # Round the summary statistics to 3 decimal places
 summary_stats = summary_stats.round(2)
-st.dataframe(summary_stats, height=300)
+st.dataframe(summary_stats)
 st.markdown("""
 Dari tabel tersebut, terlihat bahwa ada satu kecamatan yang memiliki luas yang sangat kecil, hanya 2,03 km^2, sementara kecamatan terluas mencapai 24,24 km^2. Selanjutnya, rentang kepadatan penduduk dari yang terendah, 3336 orang per km^2, hingga tertinggi, 22018 orang per km^2. Data pendidikan mencakup berbagai tingkat sekolah yang setara, memberikan gambaran yang lebih komprehensif. Terdapat indikasi adanya beberapa nilai yang terletak jauh dari rentang nilai minimum hingga maksimum pada beberapa variabel, menunjukkan kemungkinan adanya pencilan (outlier). Keberadaan pencilan ini, meski mempengaruhi analisis, tidak dihapus karena pentingnya mempertimbangkan semua kecamatan dalam evaluasi data.
             """)
