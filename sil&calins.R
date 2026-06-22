fba8eef8ecd42529977de8ec6552ca74694ff533
Nur Muhammad Herlim <99372078+aamu14@users.noreply.github.com>
2026-06-22 07:55:58 +0700

    Update sil&calins.R

--- sil&calins.R
+++ sil&calins.R
@@ -6,8 +6,16 @@ library(knitr, quietly = T)
 suppressMessages(library(dplyr))
 suppressMessages(library(reshape2))
 
+# === TAMBAHKAN DI BARIS PALING ATAS ===
+cat("=== R SCRIPT STARTED ===\n")
+cat("Working Directory:", getwd(), "\n")
+print(list.files())   # lihat file apa saja yang ada di folder
+cat("=======================\n")
+
 script_dir <- dirname(sys.frame(1)$ofile)
 setwd(script_dir)
+
+
 
 df_real <- read.csv("datatest.csv", header = TRUE, sep = ",")
 df <- df_real[,-1]
